add67e89954d4929b1ac716019cd1df2ae34b0ac
JeffGuo <85876521@qq.com>
2017-04-18 00:14:30 +0800

    调整一下

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,22 @@
 <template>
     <div>
-        <month-Select :changeSegment="changeMonth"></month-Select>
+        <month-Select :changeSegment="changeDate"></month-Select>
+        <week-Select :changeSegment="changeDate"></week-Select>
     </div>
 </template>
 
 <script>
 import monthSelect from './components/month'
-
+import weekSelect from './components/week'
 export default {
     methods:{
         //回调选择的月份段
-        changeMonth(val){
+        changeDate(val){
             console.log(val) //["2016-03-01","2016-03-31"]
         }
     },
     components: {
-        monthSelect
+        monthSelect,weekSelect
     }
 }
 </script>
@@ -24,5 +25,25 @@ export default {
     *{margin:0; padding: 0}
     body{
         font: 14px Arial, Helvetica, sans-serif
+    }
+    .time_bucket{
+        height: 45px;
+        display: flex;
+        justify-content:space-between;
+        align-items:center;
+        background: #1D8CE0;
+        color: #fff;
+        margin-bottom: 3px
+    }
+    .time_bucket > div{
+        line-height: 45px;
+        padding: 0 10px
+    }
+    .time_bucket > div:active{
+        background: #2c94e2
+    }
+    .time_bucket .none{
+        pointer-events: none;
+        color: #ccc
     }
 </style>
